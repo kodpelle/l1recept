56a34324e5d2451e251427febaeb52b6547767d8
Pelle Wiklund <pellewiklund@outlook.com>
2025-09-13 16:40:03 +0200

    feat: implement AuthContext and AuthProvider for user authentication

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,53 @@
+import type { ReactNode } from "react";
+import { createContext, useContext, useState, } from "react";
+import { login as loginRequest, logout as logoutRequest } from "../services/auth";
+
+//typescript interface som beskriver en användare
+export interface User {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+}
+
+//vad kontexten ska innehålla
+interface AuthContextType {
+  user: User | null;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+//skapa kontexten med defaultvärde undefined
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+//komponent som tillhandahåller kontexten till sina barn, dvs resten av appen, och hanterar inloggning och utloggning av användare
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  //funktion för att logga in en användare, anropar loginRequest(fetch mot backend) och uppdaterar user state
+  const login = async (email: string, password: string) => {
+    const user = await loginRequest(email, password);
+    setUser(user);
+  };
+
+  //funktion för att logga ut en användare, anropar logoutRequest(fetch mot backend) och sätter user state till null
+  const logout = async () => {
+  await logoutRequest();
+  setUser(null);
+};
+
+//tillhandahåller kontextvärdena till barnkomponenter
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+//custom hook för att använda auth kontexten i komponenter
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  return context;
+}
