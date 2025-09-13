7ea1e442a659b1e9b6f0ad798c810a1dc942613f
Pelle Wiklund <pellewiklund@outlook.com>
2025-09-13 20:03:43 +0200

    barebones header where you can see if you're logged in and if you are a button to logout.

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -1,3 +1,19 @@
+import { useAuth } from "../context/AuthContext";
+
 export default function Header() {
-  return <h1 style={{ color: "green" }}>Header Component</h1>;
+  const { user, logout } = useAuth();
+  return(
+
+    <header>
+    {user ? (
+      <>
+        <span>Inloggad som {user.email}</span>
+        <button onClick={logout}>Logga ut</button>
+        </>
+    ) : (
+      <a href="/login">Logga in</a>
+      
+    )}  
+  </header>
+  );
 }
