9923e25162db71206ff5189e4c00ce1893712d2e
Pelle Wiklund <pellewiklund@outlook.com>
2025-09-13 14:33:49 +0200

    simple form layout for login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,28 @@
 
+ function LoginPage() {
+  return (
+    <>
+    <div>
+      <h1>Login</h1>
+      <form action="login">
+        <label>
+          Email
+        <input type="text" name="email" />
+        </label>
+        <label>
+          Password
+        <input type="password" name="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+    </>
+  );
+}
 LoginPage.route = {
   path: '/login',
   menuLabel: 'Login',
   index: 1  
 };
 
-export default function LoginPage() {
-  return <h1 style={{ color: "blue" }}>Login Page</h1>;
-}
+export default LoginPage;
