5b48dbef696a1cf0a330ef4c501545d9945f7a9d
Pelle Wiklund <pellewiklund@outlook.com>
2025-09-13 21:24:22 +0200

    added bootstrap styling to the loginpage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { Container, Row, Col, Card, Form, Button, } from "react-bootstrap";
 
 
- function LoginPage() {
+function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { login } = useAuth();
@@ -12,7 +13,7 @@ import { useAuth } from "../context/AuthContext";
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
     try {
-      await login(email, password); 
+      await login(email, password);
       navigate("/recipes");
     } catch (err) {
       console.error("Login failed:", err);
@@ -20,22 +21,45 @@ import { useAuth } from "../context/AuthContext";
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-    </form>
+
+    <Container className="d-flex flex-column align-items-center justify-content-center">
+      <Row>
+        <Col>
+          <Card className="mt-5 d-flex flex-column align-items-center justify-content-center shadow">
+            <Card.Body>
+              <Card.Title className="mb-4 text-center">Logga in</Card.Title>
+
+              <form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit" className="w-100">
+                  Login
+                </Button>
+              </form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 LoginPage.route = {
