2ebe7738c7067017472600c7d650910c219eefac
kodpelle <pellewiklund@outlook.com>
2025-09-27 10:17:41 +0200

    Add RecipesListPage component with routing configuration

---
+++ src/pages/RecipesListPage.tsx
@@ -0,0 +1,8 @@
+export default function RecipesListPage() {
+    return <div>Recipes List Page</div>;
+}
+RecipesListPage.route = {
+    path: "/recipes",
+    index: 3,
+    menuLabel: "Recipes",
+};
