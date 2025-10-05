a12101b6bb13fe18e9ec677476a41024ed53fad2
kodpelle <pellewiklund@outlook.com>
2025-10-05 13:09:19 +0200

    getting the correct recipe, if not logged in or if theres no recipe with the id, return 404

--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -1,7 +1,38 @@
+import { useAuth } from "../context/AuthContext";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NotFound from "./NotFound";
+import { getRecipeById, type Recipe } from "../services/recipes";
+
+
+
 export default function EditPage() {
+    const { id } = useParams<{ id: string }>();
+    const { user } = useAuth();
+    const [recipe, setRecipe] = useState<Recipe | null>(null);
+
+    useEffect(() => {
+        if (id) {
+            getRecipeById(Number(id))
+                .then(setRecipe)
+                .catch(console.error);
+        }
+    }, [id]);
+
+    if (!user || !id) {
+        return <NotFound />;
+    }
+
+    if (!recipe) {
+        return <div>Laddar...</div>;
+    }
+
     return (
-        <div>editpage</div>
+        <div>
+            <h1>Edit {recipe.title}</h1>
+        </div>
     );
+
 }
 
 EditPage.route = {
